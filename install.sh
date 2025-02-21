c2e4011db82c05e4ada75f9e0259796213e56883
yazoink <yazoink@firemail.cc>
2025-02-21 19:33:50 +0330

    Added Shell Script

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Install My Dwm dot
+# Author: CrypticSpider
+check_err() {
+    if [[ $? -ne 0  ]]; then
+        echo "Failed"
+    else
+        echo "Succes"
+    fi
+}
+git clone https://github.com/CrypticSpider/dwm-dots.git ; clang-check && cd dwm-dots \
+    cp -R dwm/* ~/.config/dwm/;check_err \
+    cp -R dwmblocks/* ~/.config/demblocks/; check_err \
+    cp -R statusbar/ ~/.local/bin/; check_err \
+make -C ~/.config/dwm clean install && make -C ~/.config/dwmblocks clean install; check_err
+read -p "Do you want to use .xinirc?" REPLY
+if [ $REPLY = "y" ]; then
+    cp .xinirc ~/.
+else
+    echo "Install Finished"
+fi
